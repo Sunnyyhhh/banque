a710d51809ee44884d46a818588f2c83e797ab74
Sunnyyhhh <miarishouranarivelo@gmail.com>
2025-07-07 23:04:27 +0300

    modifComit

--- ws/index.php
+++ ws/index.php
@@ -3,6 +3,7 @@ require 'vendor/autoload.php';
 require 'db.php';
 require 'routes/etudiant_routes.php';
 require 'routes/utilisateur_routes.php';
+require 'routes/typePret_routes.php';
 require 'routes/etablissement_routes.php';
 
 require 'routes/pret_admin_routes.php';
